1654362be9b2985ed29bd4276606aafc2aad0185
luke <luc.dewit@hotmail.nl>
2020-10-01 10:34:22 +0200

    improved various things in the program
    - better *nix  support
    the screen was cleared with "CLS" which i changed to "cls | clear" so it will work on both windows and *nix (mac/linux)
    
    - improved program structure
    combined some lines that could easily be written on 1 line without loosing readability
    removed some extra whitespaces
    added some extra whitespaces where needed
    replaced the is keyword with == so its the same everywhere in the program

--- main.py
+++ main.py
@@ -5,22 +5,17 @@ import time
 
 # Computer options
 options = ["rock", "paper", "scissors"]
-os.system("CLS")
+os.system("cls | clear")
 
 # User input
-print("rock, paper or scissors?")
-guess = input()
-os.system("CLS")
+guess = input("rock, paper or scissors?\n")
+os.system("cls | clear")
 
-
-
-# 
 while not guess in options:
     print("Please choose rock, paper or scissors")
     guess = input()
 
-computer = math.floor(random.random()*len(options))
-computer = options[computer]
+computer = options[math.floor(random.random()*len(options))]
 
 # Rock, paper, scissors! (countdown)
 time.sleep(1)
@@ -30,37 +25,38 @@ print("Paper, ")
 time.sleep(1)
 print("Scissors")
 time.sleep(1)
-os.system("CLS")
+os.system("cls | clear")
 print("Shoot!\n")
 
 # Rock
 if guess == "rock":
-    if computer is "rock":
+    if computer == "rock":
         print("Draw!")
 
-    elif computer is "paper":
+    elif computer == "paper":
         print("Rock looses to paper, you loose!")
 
     else:
         print("Rock beats scissors, you win!")
+
 # Paper
 elif guess == "paper":
-    if computer is "rock":
+    if computer == "rock":
         print("Paper beats rock, you win!")
     
-    elif computer is "paper":
+    elif computer == "paper":
         print("Draw!")
 
     else:
         print("Paper loses to scissors, you loose!")
+
 # Scissors
 else:
-    if computer is "rock":
+    if computer == "rock":
         print("Scissors looses to rock, you loose!")
 
-    elif computer is "paper":
+    elif computer == "paper":
         print("Scissors beats paper, you win!")
 
     else:
         print("Draw!")
-
