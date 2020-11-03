43570525596d3990f0c0e5e784a1b20d7587d988
xl3ehindTim <32740573+xl3ehindTim@users.noreply.github.com>
2020-11-03 08:51:54 +0100

    Update main.py
    Split code into functions

--- main.py
+++ main.py
@@ -1,62 +1,11 @@
-import random
-import os
-import math
-import time
+from functions import computer, check_input, user, countdown, winner
 
-# Computer options
-options = ["rock", "paper", "scissors"]
-os.system("cls | clear")
+# Get computer and user action
+computer_action = computer()
+user_action = check_input(user())
 
-# User input
-guess = input("rock, paper or scissors?\n")
-os.system("cls | clear")
+# Initiate countdown
+countdown()
 
-while not guess in options:
-    print("Please choose rock, paper or scissors")
-    guess = input()
-
-computer = options[math.floor(random.random()*len(options))]
-
-# Rock, paper, scissors! (countdown)
-time.sleep(1)
-print("Rock, ")
-time.sleep(1)
-print("Paper, ")
-time.sleep(1)
-print("Scissors")
-time.sleep(1)
-os.system("cls | clear")
-print("Shoot!\n")
-
-# Rock
-if guess == "rock":
-    if computer == "rock":
-        print("Draw!")
-
-    elif computer == "paper":
-        print("Rock looses to paper, you loose!")
-
-    else:
-        print("Rock beats scissors, you win!")
-
-# Paper
-elif guess == "paper":
-    if computer == "rock":
-        print("Paper beats rock, you win!")
-    
-    elif computer == "paper":
-        print("Draw!")
-
-    else:
-        print("Paper loses to scissors, you loose!")
-
-# Scissors
-else:
-    if computer == "rock":
-        print("Scissors looses to rock, you loose!")
-
-    elif computer == "paper":
-        print("Scissors beats paper, you win!")
-
-    else:
-        print("Draw!")
+# Get winner/draw
+winner(user_action, computer_action)
