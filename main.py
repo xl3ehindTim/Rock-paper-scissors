354763edaa4c0f545ae99d2f890170ffb8335ee6
xl3ehindTim <32740573+xl3ehindTim@users.noreply.github.com>
2020-12-21 18:53:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from functions import computer, check_input, user, countdown, winner
 
-# Get computer and user action
-computer_action = computer()
+# Get user action
 user_action = check_input(user())
 
 # Initiate countdown
 countdown()
 
 # Get winner/draw
-winner(user_action, computer_action)
+winner(user_action, computer())
